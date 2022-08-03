9a34144dfeed935ce87dcc8400db42afb0a6fe22
Aashish Singla <70276198+aashishsingla567@users.noreply.github.com>
2022-08-03 13:34:15 +0530

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -4,10 +4,6 @@
 #define GAME_HPP_INCLUDED
 
 // IO
-// This is a personal academic project. Dear PVS-Studio, please check it.
-
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
-
 #include <iostream>
 
 // DS
